04adcf6e6c66bd98d89ce79445a1014a3004e7de
Jan Esser <89969152+jan-esser-git@users.noreply.github.com>
2023-08-30 13:01:37 +0200

    Display Posts & sort Posts by Date

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  safelist: [
+    'bg-yellow-600',
+    'bg-blue-600',
+    'bg-green-600',
+    'bg-purple-600',
+    'bg-red-600',
+  ]
+}
