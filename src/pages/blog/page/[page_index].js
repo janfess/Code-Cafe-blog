d0ae525fff888ad4f1808400537d86a54157339d
Jan Esser <89969152+jan-esser-git@users.noreply.github.com>
2023-08-31 20:18:53 +0200

    Refactored file.map to getPosts

--- src/pages/blog/page/[page_index].js
+++ src/pages/blog/page/[page_index].js
@@ -1,11 +1,10 @@
 import fs from 'fs'
 import path from 'path';
-import matter from 'gray-matter'
+import { getPosts } from '../../../lib/posts';
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
 import Pagination from '@/components/Pagination';
 import { POSTS_PER_PAGE } from '../../../config/index';
-import { sortByDate } from '@/utils';
 
 
 export default function Blog({ posts, numPages, currentPage }) {
@@ -43,25 +42,11 @@ export async function getStaticPaths() {
 export async function getStaticProps({params}) {
   const page = parseInt((params && params.page_index) || 1);
   const files = fs.readdirSync(path.join('posts'));
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts()
 
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
   const pageIndex = page - 1;
   const orderedPosts = posts
-    .sort(sortByDate)
     .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
  
   return {
