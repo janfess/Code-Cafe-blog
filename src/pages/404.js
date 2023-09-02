902a75a524a7e60c201ebde129c7a0873c47ac7d
Jan Esser <89969152+jan-esser-git@users.noreply.github.com>
2023-09-02 20:02:01 +0200

    Search

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,20 +7,20 @@ export default function NotFoundPage() {
   return (
     <Layout title='Page not found'>
       <div className='flex flex-col items-center mt-20'>
-        {/* <Image
-          src='/images/logo.png'
-          width={70}
-          height={70}
-          className='bg-gray-800 rounded-2xl'
-          alt='logo.png'
-        /> */}
-         <img
+        <Image
           src='/images/logo.png'
           width={70}
           height={70}
           className='bg-gray-800 rounded-2xl'
           alt='logo.png'
         />
+         {/* <img
+          src='/images/logo.png'
+          width={70}
+          height={70}
+          className='bg-gray-800 rounded-2xl'
+          alt='logo.png'
+        /> */}
         <h1 className='text-6xl my-5'>Upsala!</h1>
         <h2 className='text-4xl text-gray-400 mb-5'>This page does not exist</h2>
       </div>
